38a84fd53686b0aaedfa80805cc70b5b182c9a1d
omerasaf <balikciomerasaf@gmail.com>
2024-09-17 00:26:39 +0300

    service structure has changed

--- backend/auth-service/src/main/java/com/lab/backend/auth/controller/UserController.java
+++ backend/auth-service/src/main/java/com/lab/backend/auth/controller/UserController.java
@@ -9,6 +9,8 @@ import com.lab.backend.auth.utilities.exceptions.RedisOperationException;
 import com.lab.backend.auth.utilities.exceptions.UnauthorizedException;
 import com.lab.backend.auth.utilities.exceptions.UsernameExtractionException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -23,7 +25,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) throws RedisOperationException {
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid AuthRequest authRequest) throws RedisOperationException {
         var tokens = this.userService.login(authRequest);
         var authResponse = new AuthResponse(tokens.get(0), tokens.get(1), AuthStatus.LOGIN_SUCCESS);
         return ResponseEntity.status(HttpStatus.OK).body(authResponse);
@@ -43,13 +45,13 @@ public class UserController {
     }
 
     @PostMapping("/change-password")
-    public ResponseEntity<String> changePassword(HttpServletRequest request, @RequestBody PasswordRequest passwordRequest) throws RedisOperationException, UnauthorizedException {
+    public ResponseEntity<String> changePassword(HttpServletRequest request, @RequestBody @Valid PasswordRequest passwordRequest) throws RedisOperationException, UnauthorizedException {
         String response = this.userService.changePassword(request, passwordRequest);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PostMapping("/initiate-password-reset")
-    public ResponseEntity<String> initiatePasswordReset(@RequestParam("email") String email) {
+    public ResponseEntity<String> initiatePasswordReset(@RequestParam("email") @Email(message = "It must be a valid email") String email) {
         this.userService.initiatePasswordReset(email);
         return ResponseEntity.status(HttpStatus.OK).body("Password reset initiated. Check your email for further instructions.");
     }
