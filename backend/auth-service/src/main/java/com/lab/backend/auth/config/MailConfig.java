954ea1d06ceb69dbb384afec39614a1cd8db9ffe
omerasaf <balikciomerasaf@gmail.com>
2024-09-24 02:43:57 +0300

    improved config server

--- backend/auth-service/src/main/java/com/lab/backend/auth/config/MailConfig.java
+++ backend/auth-service/src/main/java/com/lab/backend/auth/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.lab.backend.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,13 +10,19 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("asafmarket.ltd@gmail.com");
-        mailSender.setPassword("ppqpdaaalguposcr");
+        mailSender.setUsername(this.username);
+        mailSender.setPassword(this.password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
